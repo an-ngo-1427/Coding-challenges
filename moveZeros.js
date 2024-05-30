0b8bca13fd91e33b024ab30352a2244f81e2cc19
An Ngo <anng0149@gmail.com>
2024-05-30 10:08:02 -0700

    problems

--- moveZeros.js
+++ moveZeros.js
@@ -1,30 +1,15 @@
 // URL:https://leetcode.com/problems/move-zeroes/description/
-var moveZeroes = function(nums) {
-    // let mainLen = nums.length
-    // let mainIndx = 0
-    // while(mainIndx < mainLen){
-    //     if(nums[mainIndx] === 0){
-    //         for (let j=0;j<nums.length-1;j++){
-    //             if(nums[j] === 0){
-    //                 nums[j] = nums[j+1]
-    //                 nums[j+1] = 0;
-    //             }
-    //         }
-    //         mainLen--;
-    //     }else{
-    //         mainIndx++;
-    //     }
-
-
-    // }
-
-    let zeros = 0
+const moveZeroes = function(nums) {
+    let l=0
     for(let i=0;i<nums.length;i++){
-        if(nums[i] == 0) zeros++;
-        else if(zeros > 0){
-            nums[i-zeros] = nums[i]
-            nums[i] = 0;
+       if (nums[i]){
+        const temp = nums[i]
+        nums[i] = nums[l]
+        nums[l] = temp
+        l++;
         }
     }
     return nums
 };
+
+console.log(moveZeroes([0,1,0,3,12]))
