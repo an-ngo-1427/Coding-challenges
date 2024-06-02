69df40c801c8fa4dfdc9fe9b7402dd1ccffa64cd
An Ngo <anng0149@gmail.com>
2024-06-01 21:26:11 -0700

    problem

--- rotateImage.js
+++ rotateImage.js
@@ -1,11 +1,4 @@
 //URL:https://leetcode.com/problems/rotate-image/description/
 var rotate = function (matrix) {
-    let n = matrix.length;
-    let arr = JSON.parse(JSON.stringify(matrix));
-    for (let i = 0; i < n; i++) {
-        for (let j = 0; j < n; j++) {
-            matrix[i][j] = arr[n - 1 - j][i];
-        }
-    }
-    return matrix;
+
 };
