cd90c9663754aed993a62a5753072caf7c3a0b29
An Ngo <anng0149@gmail.com>
2024-06-01 21:28:06 -0700

    problem

--- containsDuplicates.js
+++ containsDuplicates.js
@@ -1,15 +1,6 @@
 // URL: https://leetcode.com/problems/contains-duplicate/
 var containsDuplicate = function(nums) {
-    let map = {}
-    for  (let num of nums){
-        if(!map[num]) {
 
-            map[num] = 1
-            console.log(map[num])
-        }
-        else return true
-    }
-    return false
 };
 
 containsDuplicate([1,2,3,1])
