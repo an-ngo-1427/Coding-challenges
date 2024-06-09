0763f9ceff68f9dd27802e3f3ea995eb7c68d296
An Ngo <anng0149@gmail.com>
2024-06-08 20:10:54 -0700

    problem

--- findDupNum.js
+++ findDupNum.js
@@ -1,24 +1,11 @@
 //URL:https://leetcode.com/problems/find-the-duplicate-number/description/
 
 var findDuplicate = function(nums) {
-    let slow = nums[0];
-    let fast = nums[0];
-
-    while (true) {
-        slow = nums[slow];
-        fast = nums[nums[fast]];
-
-        if (slow === fast) {
-            break;
+    for(let i=0;i<nums.length-1;i++){
+        for (let j=i+1;j<nums.length;j++){
+            if(nums[i] === nums[j]) return nums[i]
         }
     }
-
-    let slow2 = nums[0];
-
-    while (slow !== slow2) {
-        slow = nums[slow];
-        slow2 = nums[slow2];
-    }
-
-    return slow;
 };
+
+console.log(findDuplicate([3,1,3,4,2]))
