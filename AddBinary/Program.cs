d948e45ecbf541baf7471d81be9c6b5e0e6635ea
An Ngo <anng0149@gmail.com>
2024-06-14 11:46:07 -0700

    solution

--- AddBinary/Program.cs
+++ AddBinary/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Solution {
+    public string AddBinary(string a, string b) {
+        int i = a.Length - 1;
+        int j = b.Length - 1;
+        int carry = 0;
+        string result = "";
+        while(i >= 0 || j >= 0){
+            int sum = carry;
+            carry = 0;
+            if (i>=0){
+                sum += a[i] - '0';
+            }
+            if (j>=0){
+                sum += b[j] - '0';
+            }
+            if(sum >= 2){
+                result += '0';
+                carry = 1;
+            }else{
+                result += sum.ToString();
+            }
+
+            i--;
+            j--;
+        }
+        if (carry > 0){
+            result += carry.ToString();
+        }
+
+        string reverseString = "";
+        for (int k=result.Length-1; k>= 0;k--){
+            reverseString += result[k];
+        }
+
+
+        return reverseString;
+    }
+}
