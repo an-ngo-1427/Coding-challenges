f4a42f6ea5e67b00983d213a5c3db28f3870223d
An Ngo <anng0149@gmail.com>
2024-06-17 08:20:29 -0700

    challenge

--- AddBinary/Program.cs
+++ AddBinary/Program.cs
@@ -5,21 +5,17 @@
         int carry = 0;
         string result = "";
         while(i >= 0 || j >= 0){
-            int sum = carry;
-            carry = 0;
-            if (i>=0){
+            int sum = 0;
+            if(i >= 0){
                 sum += a[i] - '0';
             }
-            if (j>=0){
+            if(j >= 0){
                 sum += b[j] - '0';
             }
-            if(sum >= 2){
-                result += '0';
-                carry = 1;
-            }else{
-                result += sum.ToString();
-            }
-
+            sum += carry;
+            string bin = (sum % 2).ToString();
+            carry = sum / 2;
+            result += bin;
             i--;
             j--;
         }
