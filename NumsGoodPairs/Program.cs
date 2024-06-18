75c149db44855d3225acfc7bf15418937ebe1716
An Ngo <anng0149@gmail.com>
2024-06-18 10:35:24 -0700

    problem

---
+++ NumsGoodPairs/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+   int NumIdenticalPairs(int[] nums) {
+        int count = 0;
+        for(int i=0;i<nums.Length;i++){
+            for(int j=i+1;j<nums.Length;j++){
+                if(nums[i] == nums[j]){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    Console.WriteLine(NumIdenticalPairs([1,2,3,1,1,3]));
