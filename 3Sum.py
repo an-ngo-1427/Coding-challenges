7071502629fcb71ff8f1ac70e2f1cf3336d778be
An <anng0149@gmail.com>
2024-10-12 12:57:06 -0700

    3sum

--- 3Sum.py
+++ 3Sum.py
@@ -5,21 +5,34 @@ class Solution:
         i = 0
         j = 0
         k = len(nums) - 1
-        print('this is num',nums)
         while(i < len(nums) - 1):
-            print('this is sorted',i)
-            j = i + 1
+            while(0<i<len(nums)-1 and sortedNums[i] == sortedNums[i - 1]):
+                i+=1
 
-            while(j != k):
+            j = i + 1
+            print('this is sorted',i,j)
+
+            while(j < k):
+                print('this is num',j,k)
                 total = sortedNums[i] + sortedNums[j] + sortedNums[k]
-                print('total',total)
+
                 if(total == 0):
                     res.append([sortedNums[i],sortedNums[j],sortedNums[k]])
-                    break
+                    j+= 1
+                    while(j < k and sortedNums[j] == sortedNums[j-1]):
+                        j += 1
+                    
+                    k -= 1
+                    while(k>j and sortedNums[k] == sortedNums[k+1]):
+                        k -= 1
                 elif(total < 0):
                     j += 1
+                    while(j<k and sortedNums[j] == sortedNums[j-1]):
+                        j+= 1
                 elif(total > 0):
                     k -= 1
+                    while(k>j and sortedNums[k] == sortedNums[k+1]):
+                        k-= 1
 
             k = len(nums) - 1
             i += 1
