e742df0716266ca053bb589b9dcc76ce5d3767a5
An <anng0149@gmail.com>
2024-10-14 13:29:47 -0700

    productofarray

--- ProductofArrNoSelf.py
+++ ProductofArrNoSelf.py
@@ -4,7 +4,6 @@ class Solution:
         ans = [0] * len(nums)
         leftProduct = 1
         for i in range(len(nums)):
-            print('leftproducts',leftProduct)
             if(i == 0):
                 ans[i] = leftProduct
             else:
@@ -13,10 +12,8 @@ class Solution:
 
         rightProduct = 1
         for j in reversed(range(len(nums))):
-            print('rightproducts',rightProduct)
             ans[j] = ans[j] * rightProduct
             rightProduct = rightProduct * nums[j]
 
-        print(ans)
         return ans
     productOfArray([1,2,3,4])
