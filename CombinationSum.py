7ba2a56f67c22ff22a49718b98f4f9d84731570b
An <anng0149@gmail.com>
2024-10-23 11:53:47 -0700

    comb

---
+++ CombinationSum.py
@@ -0,0 +1,30 @@
+class Solution:
+    def combinationSum(candidates,target):
+        sortedCandidates = sorted(candidates)
+        tempSum = 0
+        comb = []
+        ans = []
+
+        for num in sortedCandidates:
+            while(tempSum < target):
+                tempSum += num
+                if(tempSum == target):
+                    comb.append(num)
+                    ans.append(comb)
+                    tempSum = 0
+                    comb = []
+                    break
+                if(tempSum > target):
+                    tempSum -= 2*num
+                    if(tempSum < 0):
+                        return ans
+                    comb.pop()
+                    break
+
+                comb.append(num)
+
+        print(ans)
+
+    combinationSum([2,3,6,7],7)
+    combinationSum([2,3,5],8)
+    combinationSum([2],1)
