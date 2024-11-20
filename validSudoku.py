208e80fd1e4a875d2efafca9aa43688990790ea1
An Ngo <anng0149@gmail.com>
2024-11-20 14:53:56 -0800

    valid sudoku

--- validSudoku.py
+++ validSudoku.py
@@ -1,3 +1,4 @@
+class Solution(object):
     def isValidSudoku(self, board):
         """
         :type board: List[List[str]]
@@ -6,8 +7,11 @@
         rowDict = {}
         colDict = {}
         subBoxDict = {}
+        boxCoorDict = {}
         for i in range(len(board)):
             for j in range(len(board[i])):
+                if board[i][j] == '.':
+                    continue
                 if j not in colDict:
                     colDict[j] = []
                     colDict[j].append(board[i][j])
@@ -21,12 +25,17 @@
                     rowDict[i] = [board[i][j]]
                 else:
                     if(board[i][j] in rowDict[i] and board[i][j] != '.'):
-                        print(board[i][j])
-                        print('enterewd',rowDict)
                         return False
                     else:
                         rowDict[i].append(board[i][j])
 
-                boxCoor = [i//3,j//3]
+                boxCoor = (i//3,j//3)
                 if(boxCoor not in subBoxDict):
-                    subBoxDict[boxCoor]
+                    subBoxDict[boxCoor] = [board[i][j]]
+                else:
+                    if(board[i][j] in subBoxDict[boxCoor] and board[i][j] != '.'):
+                        return False
+                    else:
+                        subBoxDict[boxCoor].append(board[i][j])
+            
+        return True
