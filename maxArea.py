b05d96872142b3cba7b6676e6f5852ef804eacdb
An Ngo <anng0149@gmail.com>
2025-01-31 13:26:19 -0800

    most water contained

--- maxArea.py
+++ maxArea.py
@@ -4,14 +4,29 @@ class Solution(object):
         :type height: List[int]
         :rtype: int
         """
-        maxArea = float('-inf')
+        left = 0
+        right = len(height) - 1
 
-        for i in range(len(height) - 1):
-            leftSide = height[i]
-            for j in range(i+1,len(height)):
-                rightSide = height[j]
-                currVol = min(leftSide,rightSide) * (j - i)
-                if(currVol > maxArea):
-                    maxArea = currVol
+        maxArea = float('-inf')
+        while(left< right):
+            currArea = min(height[left],height[right]) * (right - left)
+            if currArea > maxArea:
+                maxArea = currArea
+
+            if height[left] < height[right]:
+                left = left + 1
+            elif height[left] >= height[right]:
+                right = right - 1
 
         return maxArea
+        # maxArea = float('-inf')
+
+        # for i in range(len(height) - 1):
+        #     leftSide = height[i]
+        #     for j in range(i+1,len(height)):
+        #         rightSide = height[j]
+        #         currVol = min(leftSide,rightSide) * (j - i)
+        #         if(currVol > maxArea):
+        #             maxArea = currVol
+
+        # return maxArea
