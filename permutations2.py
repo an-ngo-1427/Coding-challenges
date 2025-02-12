cf1ce341c48e86038bf81f25c91277f3429e7f93
An <anng0149@gmail.com>
2025-02-12 14:57:02 -0800

    permutations

--- permutations2.py
+++ permutations2.py
@@ -1,4 +1,27 @@
-nums=[[1,2,3],[3,2,1]]
+class Solution(object):
+    def permuteUnique(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        usedNum = [0] * len(nums)
+        def backtrack(ans,tempList,usedNum,nums):
+            if (len(tempList) == len(nums)):
+                print(tempList)
+                if tempList not in ans:
+                    ans.append(tempList[:])
+                return
 
-if([1,2,1] in nums):
-    print('true')
+            for i in range(len(nums)):
+                if usedNum[i]:
+                    continue
+                tempList.append(nums[i])
+                usedNum[i] = 1
+                backtrack(ans,tempList,usedNum,nums)
+                tempList.remove(nums[i])
+                usedNum[i] = 0
+
+        ans,tempList = [],[]
+        backtrack(ans,tempList,usedNum,nums)
+
+        return ans
