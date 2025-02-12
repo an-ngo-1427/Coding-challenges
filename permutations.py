56427e28744ada78321173d889403bdfb1bb3ac0
An <anng0149@gmail.com>
2025-02-12 12:40:17 -0800

    back tracking

--- permutations.py
+++ permutations.py
@@ -4,7 +4,7 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        ans,sol = [],[]
+        """ ans,sol = [],[]
         def backtrack():
             if(len(sol) == len(nums)):
                 ans.append(sol[:])
@@ -14,6 +14,23 @@ class Solution(object):
                     sol.append(nums[i])
                     backtrack()
                     sol.pop()
-        
+
         backtrack()
+        return ans """
+
+        def backtrack(ans,tempList,nums):
+            if len(tempList) == len(nums):
+                ans.append(tempList[:])
+                return
+            for i in range(len(nums)):
+                if nums[i] not in tempList:
+                    tempList.add(nums[i])
+                backtrack(ans,tempList,nums)
+                tempList.pop()
+
+        ans,tempList = [],[]
+        backtrack(ans,tempList,nums)
+
         return ans
+
+    permute([1,2,3])
